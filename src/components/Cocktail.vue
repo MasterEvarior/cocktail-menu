4ef2d05f42d286724ab7c2e7e61224914ea0450a
MasterEvarior <masterevarior@protonmail.com>
2020-10-03 21:07:22 +0200

    Functionaly implemented single cocktails

--- src/components/Cocktail.vue
+++ src/components/Cocktail.vue
@@ -1,20 +1,30 @@
 <template>
-    <div>
-        {{this.cocktail}}
-        {{this.ingredientsString}}
-    </div>
+  <div>
+    <v-row>
+      {{ this.cocktail.name }}
+    </v-row>
+    <v-row>
+      {{ this.cocktail.description }}
+    </v-row>
+    <v-row>
+      {{ this.ingredientsString }}
+    </v-row>
+    <v-row>
+        <v-divider/>
+    </v-row>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Cocktail',
-    props:{
-        cocktail: Object
+  name: "Cocktail",
+  props: {
+    cocktail: Object,
+  },
+  computed: {
+    ingredientsString: function () {
+      return this.cocktail.ingredients.join(" | ");
     },
-    computed: {
-        ingredientsString: function() {
-            return "lo";
-        }
-    }
-}
+  },
+};
 </script>
--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,23 +1,35 @@
 <template>
   <v-container class="category">
-    <SubCategory v-for="subCategory in this.subCategories" :key="subCategory.key" v-bind:subCategory='subCategory'/>
+    <row v-for="cocktail in category.cocktails" :key="cocktail.name">
+      <Cocktail :cocktail="cocktail"/>
+    </row>
+    <row>
+      <SubCategory />
+    </row>
   </v-container>
 </template>
 
 <script>
 import { categories } from "../assets/cocktails.json";
+import Cocktail from "../components/Cocktail";
 import SubCategory from "../components/SubCategory";
 
 export default {
   name: "Category",
   components: {
-    SubCategory
+    SubCategory,
+    Cocktail,
   },
   computed: {
-    subCategories:  function(){
-      return categories[this.$route.params.category];
-    }
-  }
+    category: function () {
+      for (var i in categories) {
+        if (categories[i].url === this.$route.params.category) {
+          return categories[i];
+        }
+      }
+      return {};
+    },
+  },
 };
 </script>
 
