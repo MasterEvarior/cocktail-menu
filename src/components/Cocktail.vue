3c07c189beb4e14505b4f781399d25887d0580c4
MasterEvarior <masterevarior@protonmail.com>
2020-12-08 19:54:03 +0100

    #5 drinks can be marked as "On Request Only"

--- src/components/Cocktail.vue
+++ src/components/Cocktail.vue
@@ -2,19 +2,26 @@
   <v-card dark min-height="100%" class="pb-1">
     <v-card-title>
       <span class="title font-weight-bold">{{ this.cocktail.name }}</span>
+      <v-spacer/>
+      <v-chip-group>
+        <Label v-if="this.cocktail.requestOnly" tooltip='Only on request' icon='mdi-mail'/>
+      </v-chip-group>
     </v-card-title>
-
     <v-card-text class="headline">
       <span class="description">{{ this.cocktail.description }}</span> <br>
       <span>{{ this.ingredientsString }}</span>
     </v-card-text>
-
   </v-card>
 </template>
 
 <script>
+import Label from './Label';
+
 export default {
   name: "Cocktail",
+  components: {
+    Label
+  },
   props: {
     cocktail: Object,
   },
---
+++ src/components/Label.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-tooltip top>
+      <template v-slot:activator="{ on, attrs }">
+          <v-chip 
+            v-bind="attrs"
+            v-on="on">
+            <v-icon> {{ icon }} </v-icon>
+          </v-chip>
+      </template>
+      <span>{{ tooltip }}</span>
+    </v-tooltip>
+</template>
+
+<script>
+export default {
+    name: 'Label',
+    props: {
+        tooltip: String,
+        icon: String
+    }
+}
+</script>
